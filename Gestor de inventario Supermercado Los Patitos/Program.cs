995ee6bd4b552b6958fc35f9a0da04dbc05e2168
Eyden Su <137385937+Zepgi@users.noreply.github.com>
2024-05-11 12:41:15 -0600

    PRUEBAS DE VerifyID

--- Gestor de inventario Supermercado Los Patitos/Program.cs
+++ Gestor de inventario Supermercado Los Patitos/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,9 +15,27 @@ namespace Gestor_de_inventario_Supermercado_Los_Patitos
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+
+
+            // PRUEBA FUNCIONALIDAD VERIFICADOR ID
+
+            // cedula valida
+            VerifyID v = new VerifyID();
+            string datos = v.verifyID("703100722");
+            string[] resul = datos.Split(',');
+			MessageBox.Show("Número de cédula: " + resul[0] + "\nNombre completo: " + resul[1]);
+
+			//cedula invalida (retorna 0,0)
+			VerifyID v2 = new VerifyID();
+			string datos2 = v2.verifyID("123456789");
+			string[] resul2 = datos2.Split(',');
+			MessageBox.Show("Número de cédula: " + resul2[0] + "\nNombre completo: " + resul2[1]);
+
+
+			Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new VentanaInicioSesion());
+            
         }
     }
 }
