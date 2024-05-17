3a46b288112c397b946079bd4b3c27b7b9c813d3
Eyden S <esu@estudiantec.cr>
2024-05-16 19:13:34 -0600

    CAMBIOS EN EL query

--- Gestor de inventario Supermercado Los Patitos/Base de datos/Query-SupermercadoLosPatitos.sql
+++ Gestor de inventario Supermercado Los Patitos/Base de datos/Query-SupermercadoLosPatitos.sql
@@ -69,18 +69,7 @@ VALUES	(1,'Administrador'),
 		(3,'Administrador de Inventario'),
 		(4, 'Contador');
 
-INSERT INTO Personal 
-VALUES 
-    (2, '1990-05-15', 'Juan', 'Pérez', 'González', 'Calle Principal 123', 'jozperez@gmail.com', '1234', 123456789, 'M', 1, 1),
-    (3, '1995-08-20', 'María', 'López', 'García', 'Avenida Central 456', 'maria.lopez@email.com', '1234', 987654321, 'F', 2, 1),
-    (4, '1988-12-10', 'Pedro', 'Martínez', 'Hernández', 'Calle Secundaria 789', 'jozafatperezfer@gmail.com', '1234', 555555555, 'M', 3, 1),
-    (5, '1987-04-25', 'Ana', 'Gómez', 'Fernández', 'Paseo Peatonal 234', 'ana.gomez@email.com', '1234', 111111111, 'F', 4, 1);
-
-INSERT INTO Personal 
-VALUES 
-(6, '1990-05-15', 'Juan', 'Pérez', 'González', 'Calle Principal 123', '1', '1', 123456789, 'Masculino', 1, 1);
-
-CREATE PROCEDURE VerificarCredenciales
+CREATE PROC VerificarCredenciales
     @correo VARCHAR(150),
     @contrasenia VARCHAR(250)
 AS
@@ -115,12 +104,48 @@ SELECT * FROM Personal
 
 
 
--- PARA AGREGAR NOMBRE PRODUCTO EN ORDEN
 
-USE LOS_PATITOS
 
-DROP TABLE Lineas
+
+
+-- AJUSTES
+
 DROP TABLE Productos
+DROP TABLE Lineas
+DROP TABLE Documentos
+DROP TABLE Bitacora
+DROP TABLE Personal
+DROP TABLE Clientes
+
+CREATE TABLE Personal
+	(idTrabajador	INT PRIMARY KEY	NOT NULL,
+	fechaNacim		DATE			NOT NULL,
+	nombre			VARCHAR(150)	NOT NULL,
+	apellidoPat		VARCHAR(150)	NOT NULL,
+	apellidoMat		VARCHAR(150)	NOT NULL,
+	direccion		VARCHAR(250)	NOT NULL,
+	email			VARCHAR(150)	NOT NULL,
+	contrasenia		VARCHAR(250)	NOT NULL,
+	numTel			INT				NOT NULL,
+	genero			VARCHAR(2)		NOT NULL,
+	idRol			VARCHAR(10)		NOT NULL	FOREIGN KEY REFERENCES Roles(idRol),
+	estado			BIT				NOT NULL);
+
+
+CREATE TABLE Bitacora
+	(idTrabajador	INT			NOT NULL	FOREIGN KEY REFERENCES	Personal(idTrabajador),
+	fechaHora		DATETIME	NOT NULL
+	CONSTRAINT		PK_BITACORA	PRIMARY KEY (idTrabajador,fechaHora));
+
+CREATE TABLE Documentos
+	(idDocumento	INT		NOT NULL	PRIMARY KEY,
+	tipo			INT		NOT NULL	FOREIGN KEY REFERENCES Consecutivos(tipo),
+	fechaCreacion	DATE	NOT NULL,
+	consecutivo		INT		NOT NULL,
+	idCliente		INT,
+	idTrabajador	INT		NOT NULL	FOREIGN KEY REFERENCES Personal(idTrabajador),
+	totalImpuestos	INT		NOT NULL,
+	subtotal		INT		NOT NULL);
 
 CREATE TABLE Productos
 	(codigoProd		INT	PRIMARY KEY	NOT NULL,
@@ -137,9 +162,23 @@ CREATE TABLE Lineas
 	impuesto		INT		NOT NULL,
 	idDocumento		INT		NOT NULL	FOREIGN KEY REFERENCES	Documentos(idDocumento));
 
+ALTER TABLE Lineas
+ADD CONSTRAINT PK_Lineas PRIMARY KEY (codigoProd,idDocumento);
 
 ALTER TABLE Personal
 ALTER COLUMN genero VARCHAR(20);
+
+INSERT INTO Personal 
+VALUES 
+    (1, '1990-05-15', 'Juan', 'Pérez', 'González', 'Calle Principal 123', '1', '1', 123456789, 'M', 1, 1),
+    (2, '1990-05-15', 'Juan', 'Pérez', 'González', 'Calle Principal 123', 'jozperez@gmail.com', '1234', 123456789, 'M', 1, 1),
+    (3, '1995-08-20', 'María', 'López', 'García', 'Avenida Central 456', 'maria.lopez@email.com', '1234', 987654321, 'F', 2, 1),
+    (4, '1988-12-10', 'Pedro', 'Martínez', 'Hernández', 'Calle Secundaria 789', 'jozafatperezfer@gmail.com', '1234', 555555555, 'M', 3, 1),
+    (5, '1987-04-25', 'Ana', 'Gómez', 'Fernández', 'Paseo Peatonal 234', 'ana.gomez@email.com', '1234', 111111111, 'F', 4, 1);
+
+INSERT INTO Personal 
+VALUES 
+(6, '1990-05-15', 'Juan', 'Pérez', 'González', 'Calle Principal 123', '1', '1', 123456789, 'Masculino', 1, 1);
 
 INSERT INTO Productos (codigoProd, nombre, categoria, tipoMedida, cantidadInv, precioUnit) VALUES
 (1, 'Leche Entera', 'Lácteos', 'Litros', 50, 1.50),
@@ -155,7 +194,3 @@ VALUES (30, 'Leche de Avena', 'Lácteos', 'Unidad', 100, 2.99),
        (31, 'Avena', 'Cereales', 'Paquete', 50, 4.99),
        (32, 'Galleta Avena y fresa', 'Snacks', 'Paquete', 80, 3.49),
        (33, 'Helado de Avena y miel', 'Helados', 'Unidad', 120, 1.99);
-
-ALTER TABLE Bitacora
-ADD motivo VARCHAR(255);
-
